b3ae803d070ddd4fb8a0b666a2213f1a827e8ba2
satriaherman <muhammadsatriaherman@gmail.com>
2021-09-19 10:54:10 +0700

    create slick just on mobile version

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,17 @@ const openNav = (menu) => {
     const nav = document.querySelector('nav')
     nav.classList.toggle('open-nav')
 }
+
+window.onload = function(){
+    if(window.innerWidth < 769){
+        $('.grid-card').slick({
+            dots: true,
+            infinite: false,
+            speed: 300,
+            centerMode : true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        })
+    }
+ 
+}
