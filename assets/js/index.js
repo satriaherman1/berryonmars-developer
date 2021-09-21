21fbc81e0c3f3f6df96797799483c31fd1251d1e
satriaherman <muhammadsatriaherman@gmail.com>
2021-09-21 17:53:52 +0700

    create footer page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-const openNav = (menu) => {
+const openNav = () => {
     const nav = document.querySelector('nav')
     nav.classList.toggle('open-nav')
 }
 
 window.onload = function(){
-    if(window.innerWidth < 769){
+    if(window.matchMedia('(max-width: 768px)').matches){
         $('.grid-card').slick({
             dots: true,
             infinite: false,
@@ -12,7 +12,23 @@ window.onload = function(){
             centerMode : true,
             slidesToShow: 1,
             slidesToScroll: 1,
+            arrows: false,
         })
     }
- 
+ console.log(window.innerWidth)
 }
+
+const faqLink = document.querySelectorAll('.faq-link')
+
+const faqAction = (el) => {
+    console.log(el)
+}
+
+faqLink.forEach((faq) => {
+  
+    faq.onclick =  function(){
+       faq.querySelector('img').classList.toggle('rotate180')
+    }
+})
+
+
